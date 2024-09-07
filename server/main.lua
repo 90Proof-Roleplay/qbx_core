30d959923071b1836c0a07155dddb5e4cb8078d0
Magnitrude <100609084+Magnitrude@users.noreply.github.com>
2024-09-07 01:21:57 -0400

    update?

--- server/main.lua
+++ server/main.lua
@@ -27,7 +27,7 @@ QBX.Shared = require 'shared.main'
 ---@type table<Source, Player>
 QBX.Players = {}
 GlobalState.PlayerCount = 0
-GlobalState.MaxPlayers = GetConvarInt('sv_maxclients', 48)
+GlobalState.MaxPlayers = GetConvarInt('sv_maxclients', 128)
 
 QBX.Player_Buckets = {}
 QBX.Entity_Buckets = {}
